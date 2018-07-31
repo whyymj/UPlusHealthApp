446b90cedcebcf68508059cea9f4914ede551216
sunjian@jbinfo.cn <sunjian@jbinfo.cn>
2018-07-31 10:41:28 +0800

    手机摄像头、苹果健康档案cordova插件调用实例

---
+++ src/components/test1.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    camera测试:
+    <div>
+      <button type="button" @click='getCamera(1)'>相机</button>
+      <button type="button" @click='getCamera(0)'>图库</button>
+    </div>
+
+    苹果健康测试:
+    <div>
+      <button type="button" @click='saveSleepInfo()'>打开苹果健康</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  //  import VueCordova from 'vue-cordova'
+  export default {
+    name: "test1",
+    data(){
+      return {
+        popupVisible: false,
+        startDate: new Date(new Date().getTime() - 1 * 24 * 60 * 60 * 1000),
+        endDate: new Date,
+      }
+    },
+    methods: {
+      //相机
+      getCamera(type){
+        navigator.camera.getPicture(this.onSuccess, this.onFail, {
+          quality: 50,
+          destinationType: Camera.DestinationType.DATA_URL,
+          encodingType: Camera.EncodingType.JPEG,
+          sourceType: type
+        })
+      },
+      onSuccess(imageData){
+        resolve('data:image/jpeg;base64,' + imageData)
+      },
+      onFail(message){
+        reject(message)
+        alert(message)
+      },
+      //打开苹果健康权限
+      getHealth(){
+        let supportedTypes = ['HKCategoryTypeIdentifierSleepAnalysis'];
+        window.plugins.healthkit.requestAuthorization({
+          readTypes: supportedTypes,
+        });
+      },
+      //保存信息
+      saveSleepInfo(){
+          alert("进入保存信息方法")
+        alert("window方式调用")
+        let _this = this;
+        window.plugins.healthkit.monitorSampleType({
+          'sampleType': 'HKCategoryTypeIdentifierSleepAnalysis'
+        }, function (value) {
+            _this.getSleepInfo();
+        })
+      },
+      //获取信息
+      getSleepInfo(value1){
+        alert("开始时间："+this.startDate);
+        alert("结束时间："+this.endDate);
+        alert("sampleType："+value1);
+        window.plugins.healthkit.querySampleType({
+          'startDate': this.startDate, // 开始时间
+          'endDate': this.endDate, // now 结束时间
+          'sampleType': 'HKCategoryTypeIdentifierSleepAnalysis',
+          'limit': 1,
+          'ascending': 'T',
+        }, function (value) {
+          alert("text+++++" + JSON.stringify(value));
+        })
+      }
+    },
+    mounted(){
+      //照相机
+
+      //打开苹果健康权限
+      this.getHealth();
+
+    }
+
+  }
+</script>
