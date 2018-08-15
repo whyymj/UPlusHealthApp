da4236b22bff1d4e62e92a111d3389e49d280623
RicoLiu <2869183609@qq.com>
2018-08-15 14:44:39 +0800

    modify issue

--- config/global.config.js
+++ config/global.config.js
@@ -2,7 +2,7 @@ module.exports = {
     // host: '10.159.46.28',
     // port: '80',
     // url: 'http://healthrecord.haier.net/healthRecordsB',
-    url: 'http://123.103.113.201:8085//healthRecordsB', // app 免登录 URL 配置
+    url: 'http://123.103.113.201:8085/healthRecordsB', // app 免登录 URL 配置
     // host: '10.130.94.227',
     host: '127.0.0.1',
     port: '8088',
