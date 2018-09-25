9aa052ec5ac17461a716b39798bf9dcd21c162dd
RicoLiu <2869183609@qq.com>
2018-09-25 16:23:15 +0800

    update

--- config/global.config.js
+++ config/global.config.js
@@ -2,10 +2,10 @@ module.exports = {
     // host: '10.159.46.28',
     // port: '80',
     // url: 'http://healthrecord.haier.net/healthRecordsB',
-    url: 'http://10.130.95.125:8088/healthRecordsB', // app 免登录 URL 配置
+    url: 'http://123.103.113.201:8085/healthRecordsB', // app 免登录 URL 配置
     // host: '123.103.113.201:8085',
-    host: '10.130.94.227',//95.118
-    port: '9020',
+    host: '127.0.0.1',//95.118
+    port: '8088',
     path: '/healthcare',
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
